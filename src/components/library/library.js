4112e7f4b5aa2629661ed20425f1b1c4d5c35383
Sean <seanpayne479@gmail.com>
2018-07-12 11:54:03 -0600

    Schedule is almost done

--- src/components/library/library.js
+++ src/components/library/library.js
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import LibraryCourse from './libraryCourse';
-import { connect } from 'react-redux';
-import * as actions from '../../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as actions from "../../actions";
+
+import LibraryCourse from "./libraryCourse";
 
 class Library extends Component {
+  componentWillMount() {
+    this.props.fetchCourses();
+  }
 
-    componentWillMount() {
-        this.props.fetchCourses();
-      }
+  renderCourses() {
+    const data = this.props.courses;
 
+    return data.map((course, index) => {
+      return <LibraryCourse {...course} key={index} />;
+    });
+  }
 
-    render() {
-        
-        return (
-            <div className="library">
-                <h1 className="library__title">Course Library</h1>
-                <LibraryCourse/>
-                <LibraryCourse/>
-                <LibraryCourse/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="library">
+        <div className="library__container">
+          <h1 className="library__container__title">Course Library</h1>
+          {this.renderCourses()}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default connect(null, actions)(Library);
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps, actions)(Library);
--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-class progressTracker extends Component {
+class ProgressTracker extends Component {
+
+    calculateProgress = function() {
+        return (this.props.amountEnrolled / this.props.amountPossible) * 100;
+    }.bind(this);
+
     render() {
-        return(
+        return (
             <div className="progress-tracker">
-                <label className="progress-tracker__label">Progress Tracker</label>
+                <div className="progress-tracker__title">Progress Tracker</div>
                 <div className="progress-tracker__percentage">
-                    0%
+                    {this.calculateProgress()}%
                 </div>
             </div>
         )
     }
 }
 
-export default progressTracker;
+export default ProgressTracker;
